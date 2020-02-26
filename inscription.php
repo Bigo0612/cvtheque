40a1d4c8881f9698986c7b38a7024d5726faf4ce
Bigo0612 <quesada.steve@gmail.com>
2020-02-26 22:01:20 +0100

    start registration html css

---
+++ inscription.php
@@ -0,0 +1,33 @@
+<?php
+
+
+include ('inc/header.php');
+?>
+
+<section class="research">
+    <div class="wrap">
+        <div class="frame">
+            <div class="research_executive">
+                <a class="logo2" href="index.html"><img src="asset/img/logo.png" alt=""></a>
+                <hr>
+                <div class="research_question">
+                    <p>Que recherchez vous?</p>
+                </div>
+                <form class="form">
+                    <div class="main_search">
+                        <input type="text" class="searchJob2" placeholder="Search jobs">
+                    </div>
+                    <div class="main_search">
+                        <input type="text" class="searchJob2" placeholder="Search jobs">
+                    </div>
+                    <div class="main_search">
+                        <input type="text" class="searchJob2" placeholder="Search jobs">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="clear"></div>
+</section>
+
+<?php include('inc/footer.php');
