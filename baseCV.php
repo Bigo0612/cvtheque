970d1b4002e0be8c2738a499e346325f6aa1f10c
Bigo0612 <quesada.steve@gmail.com>
2020-02-27 16:09:41 +0100

    html and css baseCV finished

---
+++ baseCV.php
@@ -0,0 +1,80 @@
+<?php
+
+
+include ('inc/header.php');
+?>
+
+    <section class="research">
+        <div class="wrap">
+            <div class="frame">
+                    <a class="logo2" href="index.html"><img src="asset/img/logo.png" alt=""></a>
+                    <hr>
+                    <div class="research_question">
+                        <p>Base CV</p>
+                    </div>
+            </div>
+                    <form class="form">
+                        <div class="label">
+                            <label class="lab2" for="nom">Nom/Prenom</label>
+                            <label class="lab2" for="nom">Titre CV</label>
+                        </div>
+                        <div class="Arround">
+                            <div class="inscrip2">
+                                <img src="asset/img/logo.png" alt="">
+                                <input type="text" class="searchJob2">
+                                <input type="text" class="searchJob2">
+                                <input id="susub" type="submit" name="submitted" value="Voir">
+                            </div>
+                        </div>
+                        <div class="Arround">
+                            <div class="inscrip2">
+                                <img src="asset/img/logo.png" alt="">
+                                <input type="text" class="searchJob2">
+                                <input type="text" class="searchJob2">
+                                <input id="susub" type="submit" name="submitted" value="Voir">
+                            </div>
+                        </div>
+                        <div class="Arround">
+                            <div class="inscrip2">
+                                <img src="asset/img/logo.png" alt="">
+                                <input type="text" class="searchJob2">
+                                <input type="text" class="searchJob2">
+                                <input id="susub" type="submit" name="submitted" value="Voir">
+                            </div>
+                        </div>
+                        <div class="Arround">
+                            <div class="inscrip2">
+                                <img src="asset/img/logo.png" alt="">
+                                <input type="text" class="searchJob2">
+                                <input type="text" class="searchJob2">
+                                <input id="susub" type="submit" name="submitted" value="Voir">
+                            </div>
+                        </div>
+                        <div class="Arround">
+                            <div class="inscrip2">
+                                <img src="asset/img/logo.png" alt="">
+                                <input type="text" class="searchJob2">
+                                <input type="text" class="searchJob2">
+                                <input id="susub" type="submit" name="submitted" value="Voir">
+                            </div>
+                        </div>
+                        <div class="Arround">
+                            <div class="inscrip2">
+                                <img src="asset/img/logo.png" alt="">
+                                <input type="text" class="searchJob2">
+                                <input type="text" class="searchJob2">
+                                <a href="cv.php"><input id="susub" type="submit" name="submitted" value="Voir"></a>
+                            </div>
+                        </div>
+
+                    </form>
+
+
+        </div>
+        <div class="clear"></div>
+    </section>
+
+
+
+
+<?php include('inc/footer.php');
