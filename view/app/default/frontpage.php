0bf46c31e53d38e2959118a701a161d1e9dfd55c
Greugreu <fenris.fitz@hotmail.fr>
2020-03-02 12:08:02 +0100

    fix login error span + admin backoffice start

--- view/app/default/frontpage.php
+++ view/app/default/frontpage.php
@@ -2,4 +2,31 @@
     <?= $message; ?>
 </h1>
 
-<p>COUCOU C'EST LA PAGE D'ACCUEIL</p>
+<?php use App\Service\View;
+$c = new View();
+?>
+
+<section class="research">
+    <div class="wrap">
+        <div class="frame">
+            <div class="research_executive">
+                <a class="logo2" href="index.php"><img src="../public/assets/img/logo.png" alt=""></a>
+                <hr>
+                <div class="research_question">
+                    <p>Que recherchez vous?</p>
+                </div>
+                <form class="form">
+                    <div class="main_search">
+                        <input type="text" class="searchJob2" placeholder="Search jobs">
+                        <div class="btn3">
+                            <button class="btn4" type="button">
+                                <a href="<?= $c->path('databasecv') ?>"><i  class="fas fa-search"></i></a>
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="clear"></div>
+</section>
